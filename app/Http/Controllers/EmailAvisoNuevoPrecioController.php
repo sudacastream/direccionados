53b8645663e00f5b7fffebd5c9eb94005ced5954
Elías Flores <ceo@sudacastream.com>
2023-04-14 10:57:33 -0300

    proxy solve

--- app/Http/Controllers/EmailAvisoNuevoPrecioController.php
+++ app/Http/Controllers/EmailAvisoNuevoPrecioController.php
@@ -22,20 +22,32 @@ class EmailAvisoNuevoPrecioController extends Controller
         $listadoMorosos = DB::table('tickets')->where('precio','=','1499')->where('pago','=',false)->distinct()->pluck('usuario');
 
         $contenido = '';
+        $montoTotal = 0;
         foreach($listadoMorosos as $moroso)
         {
-            $token = DB::table('tickets')->where('usuario','=',$moroso)->where('precio','=','1499')->where('pago','=',false)->distinct()->pluck('token');
-            foreach($tickets as $ticket)
+            $tokens = DB::table('tickets')->where('usuario','=',$moroso)->where('precio','=','1499')->where('pago','=',false)->distinct()->pluck('token');
+            if(count($tokens) > 0)
             {
-                $contenido .= $ticket->nombres.'<br/>';
-                $contenido .= $ticket->token.'<br/>';
-            }        
-            $montoTotal = '3291';
-                
-            $contenido .= '</ul><span style="font-weight:600;">Monto total: $'.$montoTotal.'</span><ul></ul>';
-            $contenido .= '<span style="font-weight:600;">Cuenta a transferir:</span><ul style="margin-top:5px;list-style:none;">';
-            $contenido .= '<li>Titular: Gastón Eugenio Fidelibus</li><li>CVU: 0000003100021237935253</li><li>Alias: direccionados23</li><li>CUIT/CUIL: 20214537460</li><li>Mercado Pago</li></ul>';
+                foreach($tokens as $token)
+                {
+                    $contenido .= '<span style="font-weight:600;">Token Pass: '.$token.'</span><ul></ul>';
+                    $contenido .= '<span style="font-weight:600;">Detalle de la compra:</span><ul style="margin-top:5px;">';
+                    $db = DB::table('tickets')->where('token','=',$token)->get();
+                    foreach($db as $ticket)
+                    {
+                        $contenido .= '<li>Ticket: '.$ticket->nombres.' '.$ticket->apellidos.' ('.$ticket->dni.') - <span>$'.$ticket->precio.'</span>.</li>';
+                        $montoTotal = $montoTotal + $ticket->precio;
+                    }
+                    $contenido .= '</br>';
+                    $contenido .= '</ul><span style="font-weight:600;">Monto total: $'.$montoTotal.'</span><ul></ul>';
+                    $contenido .= '<span style="font-weight:600;">Cuenta a transferir:</span><ul style="margin-top:5px;list-style:none;">';
+                    $contenido .= '<li>Titular: Gastón Eugenio Fidelibus</li><li>CVU: 0000003100021237935253</li><li>Alias: direccionados23</li><li>CUIT/CUIL: 20214537460</li><li>Mercado Pago</li></ul>';
+                    $contenido .= '</br></br></br>';
+                    $montoTotal = 0;
+                }
+            }
             echo $contenido;
+            $contenido = '';
         }
         // Mail::to($email)->send(new ConfirmacionPago($request->token));
     }
