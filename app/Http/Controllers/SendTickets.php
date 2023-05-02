115eb9d4061307592a5eed92ceac219f34a9a8d2
Elías Flores <ceo@sudacastream.com>
2023-05-02 10:30:06 -0300

    ticket bg

--- app/Http/Controllers/SendTickets.php
+++ app/Http/Controllers/SendTickets.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class SendTickets extends Controller
 {
@@ -48,5 +49,43 @@ class SendTickets extends Controller
             'merchandising' => $merchandising,
             'usuarios' => $usuarios,
         ]);
+    }
+    public function send(Request $request)
+    {
+        $tokens = DB::table('tickets')->where('pago','=',true)->distinct()->pluck('token');
+        foreach($tokens as $token)
+        {
+            $tickets = DB::table('tickets')->where('token','=',$token)->get();
+            $buffet = DB::table('buffet')->where('token','=',$token)->get();
+            $merchandising = DB::table('merchandising')->where('token','=',$token)->get();
+
+            foreach($tickets as $ticket)
+            {
+                echo $ticket->token.' - '.$ticket->nombres.'</br>';
+            }
+            foreach($buffet as $combo)
+            {
+                echo $combo->token.' - '.$combo->cantidad;
+                if($combo->cantidad > 1)
+                {
+                    echo ' combos</br>';
+                }
+                else
+                {
+                    echo ' combo</br>';
+                }
+            }
+            foreach($merchandising as $merch)
+            {
+                echo $merch->token.' - '.$merch->cantidad.' '.$merch->producto.'</br>';
+            }
+            echo '</br>';
+        }
+        $data = [
+            'titulo' => 'TICKET'
+        ];
+        $customPaper = array(0,0,396.85,198.425);
+        $pdf = Pdf::loadView('pdf.ticket-pdf', $data)->setPaper($customPaper, 'landscape');
+        return $pdf->stream('archivo.pdf');
     }
 }
---
+++ resources/views/pdf/ticket-pdf.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tickecito</title>
+</head>
+<style>
+    body, html
+    {
+        background: url('https://pass.direccionados.ar/bg-ticket.png') center center;
+        background-size: cover;
+        margin: 0;
+        padding: 0;
+    }
+    .info
+    {
+        background: #fff;
+        bottom: 0;
+        height: 20%;
+        position: fixed;
+        width: 100%;
+    }
+</style>
+<body>
+    <div class="brand"></div>
+    <div class="info">
+
+    </div>
+</body>
+</html>
