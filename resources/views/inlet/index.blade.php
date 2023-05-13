5b4055b76e08bf9f2686447dd12a759061d07f36
Elías Flores <ceo@sudacastream.com>
2023-05-13 13:27:23 -0300

    no dark icon

--- resources/views/inlet/index.blade.php
+++ resources/views/inlet/index.blade.php
@@ -77,7 +77,7 @@
                                                     html += item.apellidos+', '+item.nombres+' ('+item.dni+')';
                                                     if(item.funcion == 'pastor')
                                                     {
-                                                        html += '<span title="Pastor/a" class="inline-flex items-center bg-yellow-100 text-yellow-600 text-xs font-medium p-2.5 ml-2 rounded-full dark:bg-yellow-900 dark:text-yellow-300"><i class="fa-solid fa-user-tie"></i></span>'
+                                                        html += '<span title="Pastor/a" class="inline-flex items-center bg-yellow-100 text-yellow-600 text-xs font-medium p-2.5 ml-2 rounded-full"><i class="fa-solid fa-user-tie"></i></span>'
                                                     }
                                                     html += '</th><td class="px-2 py-4">';
                                                     html += '<label class="relative inline-flex items-center cursor-pointer"><input id="cb'+item.id+'" '+checked+' onchange="asistencia('+item.id+', this.checked);" type="checkbox" value="" class="asistencia sr-only peer"><div class="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[\'\'] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-blue-600"></div></label></td></tr>';
