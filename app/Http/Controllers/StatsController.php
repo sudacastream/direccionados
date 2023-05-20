2d5489bc79b442ae5948b883b3a701f65441d65a
Elías Flores <ceo@sudacastream.com>
2023-05-20 00:00:56 -0300

    stats

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -11,66 +11,27 @@ class StatsController extends Controller
     {
         if($request->user()['email'] == 'ceo@sudacastream.com' || $request->user()['email'] == 'nahufidelibus@gmail.com')
         {
-            $iTotal = DB::table('tickets')->get();
-            $iFebrero = DB::table('tickets')->whereMonth('created_at','=','02')->get();
-            $iMarzo = DB::table('tickets')->whereMonth('created_at','=','03')->get();
-            $iAbril = DB::table('tickets')->whereMonth('created_at','=','04')->get();
-            $iMayo = DB::table('tickets')->whereMonth('created_at','=','05')->get();
-            $pagos = DB::table('tickets')->where('pago','=',true)->get();
-            $impagos = DB::table('tickets')->where('pago','=',false)->get();
-
-            $buffetPagos = DB::table('buffet')->where('pago','=',true)->get();
-            $cantidadBuffetPagos = 0;
-            foreach($buffetPagos as $buffet)
+            $totales = DB::table('tickets')->get();
+            $presentes = DB::table('tickets')->where('asistencia','=',true)->get();
+            $combos = DB::table('buffet')->get();
+            $totalCombos = 0;
+            foreach($combos as $combo)
             {
-                $cantidadBuffetPagos = $cantidadBuffetPagos + $buffet->cantidad;
+                $totalCombos += $combo->cantidad;
             }
-            $buffetImpagos = DB::table('buffet')->where('pago','=',false)->get();
-            $cantidadBuffetImpagos = 0;
-            foreach($buffetImpagos as $buffet)
+            $combosEntregados = DB::table('buffet')->where('retiro','=',true)->get();
+            $totalCombosEntregados = 0;
+            foreach($combosEntregados as $entregado)
             {
-                $cantidadBuffetImpagos = $cantidadBuffetImpagos + $buffet->cantidad;
-            }
-            $remerasPagas = DB::table('merchandising')->where('producto','LIKE','%remera%')->where('pago','=',true)->get();
-            $cantidadRemerasPagas = 0;
-            foreach($remerasPagas as $remeras)
-            {
-                $cantidadRemerasPagas = $cantidadRemerasPagas + $remeras->cantidad;
-            }
-            $remerasImpagas = DB::table('merchandising')->where('producto','LIKE','%remera%')->where('pago','=',false)->get();
-            $cantidadRemerasImpagas = 0;
-            foreach($remerasImpagas as $remeras)
-            {
-                $cantidadRemerasImpagas = $cantidadRemerasImpagas + $remeras->cantidad;
-            }
-            $gorrasPagas = DB::table('merchandising')->where('producto','LIKE','%gorra%')->where('pago','=',true)->get();
-            $cantidadGorrasPagas = 0;
-            foreach($gorrasPagas as $gorras)
-            {
-                $cantidadGorrasPagas = $cantidadGorrasPagas + $gorras->cantidad;
-            }
-            $gorrasImpagas = DB::table('merchandising')->where('producto','LIKE','%gorra%')->where('pago','=',false)->get();
-            $cantidadGorrasImpagas = 0;
-            foreach($gorrasImpagas as $gorras)
-            {
-                $cantidadGorrasImpagas = $cantidadGorrasImpagas + $gorras->cantidad;
+                $totalCombosEntregados += $entregado->cantidad;
             }
 
             return view('admin.stats', [
                 'user' => $request->user(),
-                'iTotal' => $iTotal,
-                'iFebrero' => $iFebrero,
-                'iMarzo' => $iMarzo,
-                'iAbril' => $iAbril,
-                'iMayo' => $iMayo,
-                'pagos' => $pagos,
-                'impagos' => $impagos,
-                'buffetPagos' => $cantidadBuffetPagos,
-                'buffetImpagos' => $cantidadBuffetImpagos,
-                'remerasPagas' => $cantidadRemerasPagas,
-                'remerasImpagas' => $cantidadRemerasImpagas,
-                'gorrasPagas' => $cantidadGorrasPagas,
-                'gorrasImpagas' => $cantidadGorrasImpagas,
+                'totales' => $totales,
+                'presentes' => $presentes,
+                'totalCombos' => $totalCombos,
+                'combosEntregados' => $totalCombosEntregados,
             ]);
         }
         else
--- resources/views/admin/stats.blade.php
+++ resources/views/admin/stats.blade.php
@@ -38,101 +38,49 @@
                     <div class="overflow-hidden">
                         <div class="grid grid-cols-2 lg:grid-cols-4 gap-4 pt-5 mb-5">
                             <div class="h-auto max-w-full bg-slate-50 border border-gray-200 rounded-lg shadow py-3 px-5 self-start">
-                                <span class="text-lg text-gray-900">Inscripciones</span>
+                                <span class="text-lg text-gray-900">Asistencia</span>
                                 <div class="flex items-center justify-between">
-                                    <div class="text-3xl font-bold text-gray-900 cursor-pointer" title="Porcentaje de tickets vendidos">{{ number_format(count($iTotal)*100/280, 0, ',','') }}%</div>
+                                    <div class="text-3xl font-bold text-gray-900 cursor-pointer" title="Porcentaje de personas que ingresaron">{{ number_format(count($presentes)*100/count($totales), 0, ',','') }}%</div>
                                     <div class="text-lg font-bold flex items-center justify-between">
-                                        <span class="bg-green-100 text-green-800 mr-2 px-2.5 py-0.5 rounded-full cursor-pointer" title="Tickets pagos">{{ count($pagos) }}</span>
-                                        <span class="bg-red-100 text-red-800 mr-2 px-2.5 py-0.5 rounded-full cursor-pointer" title="Tickets impagos">{{ count($impagos) }}</span>
+                                        <span class="bg-green-100 text-green-800 mr-2 px-2.5 py-0.5 rounded-full cursor-pointer" title="Presentes">{{ count($presentes) }}</span>
+                                        <span class="bg-red-100 text-red-800 mr-2 px-2.5 py-0.5 rounded-full cursor-pointer" title="Pendientes">{{ count($totales)-count($presentes) }}</span>
                                     </div>
-                                </div>  
+                                </div>
                             </div>
+                            
                             <div class="h-auto max-w-full bg-slate-50 border border-gray-200 rounded-lg shadow py-3 px-5 self-start">
                                 <span class="text-lg text-gray-900">Buffet</span>
                                 <div class="flex items-center justify-between">
-                                    <div class="text-3xl font-bold text-gray-900 cursor-pointer" title="Total de combos vendidos">{{ $buffetPagos + $buffetImpagos }}</div>
+                                    <div class="text-3xl font-bold text-gray-900 cursor-pointer" title="Total de combos vendidos">{{ $totalCombos }}</div>
                                     <div class="text-lg font-bold flex items-center justify-between">
-                                        <span class="bg-green-100 text-green-800 mr-2 px-2.5 py-0.5 rounded-full cursor-pointer" title="Combos pagos">{{ $buffetPagos }}</span>
-                                        <span class="bg-red-100 text-red-800 mr-2 px-2.5 py-0.5 rounded-full cursor-pointer" title="Combos impagos">{{ $buffetImpagos }}</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="h-auto max-w-full bg-slate-50 border border-gray-200 rounded-lg shadow py-3 px-5 self-start">
-                                <span class="text-lg text-gray-900">Remeras</span>
-                                <div class="flex items-center justify-between">
-                                    <div class="text-3xl font-bold text-gray-900 cursor-pointer" title="Total de remeras vendidas">{{ $remerasPagas + $remerasImpagas }}</div>
-                                    <div class="text-lg font-bold flex items-center justify-between">
-                                        <span class="bg-green-100 text-green-800 mr-2 px-2.5 py-0.5 rounded-full cursor-pointer" title="Remeras pagas">{{ $remerasPagas }}</span>
-                                        <span class="bg-red-100 text-red-800 mr-2 px-2.5 py-0.5 rounded-full cursor-pointer" title="Remeras impagas">{{ $remerasImpagas }}</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="h-auto max-w-full bg-slate-50 border border-gray-200 rounded-lg shadow py-3 px-5 self-start">
-                                <span class="text-lg text-gray-900">Gorras</span>
-                                <div class="flex items-center justify-between">
-                                    <div class="text-3xl font-bold text-gray-900 cursor-pointer" title="Total de gorras vendidas">{{ $gorrasPagas + $gorrasImpagas }}</div>
-                                    <div class="text-lg font-bold flex items-center justify-between">
-                                        <span class="bg-green-100 text-green-800 mr-2 px-2.5 py-0.5 rounded-full cursor-pointer" title="Gorras pagas">{{ $gorrasPagas }}</span>
-                                        <span class="bg-red-100 text-red-800 mr-2 px-2.5 py-0.5 rounded-full cursor-pointer" title="Gorras impagas">{{ $gorrasImpagas }}</span>
+                                        <span class="bg-green-100 text-green-800 mr-2 px-2.5 py-0.5 rounded-full cursor-pointer" title="Combos entregados">{{ $combosEntregados }}</span>
+                                        <span class="bg-red-100 text-red-800 mr-2 px-2.5 py-0.5 rounded-full cursor-pointer" title="Combos pendientes">{{ $totalCombos-$combosEntregados }}</span>
                                     </div>
                                 </div>
                             </div>
                         </div>
 
-
-                        <div class="">
-                            <div class="px-5 py-3 dark:text-neutral-200">
-                              Cantidad de Inscripciones
-                            </div>
-                            <canvas class="p-10" id="chartLine"></canvas>
-                            <!-- Required chart.js -->
-                            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-                            
-                            <!-- Chart line -->
-                            <script>
-                                const labels = ["Febrero", "Marzo", "Abril", "Mayo"];
-                                const data = {
-                                labels: labels,
-                                datasets: [
-                                    {
-                                    label: "Inscripciones",
-                                    backgroundColor: "rgb(3, 105, 161)",
-                                    borderColor: "hsl(217, 57%, 51%)",
-                                    data: [{{ count($iFebrero) }}, {{ count($iMarzo) }}, {{ count($iAbril) }}, {{ count($iMayo) }}],
-                                    },
-                                ],
-                                };
-                            
-                                const configLineChart = {
-                                type: "bar",
-                                data,
-                                options: {},
-                                };
-                            
-                                var chartLine = new Chart(
-                                document.getElementById("chartLine"),
-                                configLineChart
-                                );
-                            </script>
-                        </div>
+                        <!-- Required chart.js -->
+                        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                        
                         <div class="ml-0 mt-6 grid grid-cols-2">
                             <div>
                                 <div class="px-5 py-3">
-                                    Cupos
+                                    Asistencia
                                 </div>
                                 <canvas class="p-10" id="chartPie1"></canvas>
                             </div>
                             <!-- Chart pie -->
                             <script>
                                 const dataPie1 = {
-                                labels: ["Vendidos", "Disponibles"],
+                                labels: ["Pendientes", "Presentes"],
                                 datasets: [
                                     {
                                     label: "Cantidad",
-                                    data: [{{ count($iTotal) }}, 280-{{ count($iTotal) }}],
+                                    data: [{{ count($totales)-count($presentes) }},{{ count($presentes) }}],
                                     backgroundColor: [
-                                        "rgb(34, 197, 94)",
                                         "rgb(239, 68, 68)",
+                                        "rgb(34, 197, 94)",
                                     ],
                                     hoverOffset: 4,
                                     },
@@ -152,21 +100,21 @@
                             </script>
                             <div>
                                 <div class="px-5 py-3">
-                                    Pagos
+                                    Buffet
                                 </div>
                                 <canvas class="p-10" id="chartPie2"></canvas>
                             </div>
                             <!-- Chart pie -->
                             <script>
                                 const dataPie2 = {
-                                labels: ["Pagos", "Impagos"],
+                                labels: ["Pendientes", "Entregados"],
                                 datasets: [
                                     {
                                     label: "Cantidad",
-                                    data: [{{ count($pagos) }}, {{ count($impagos) }}],
+                                    data: [{{ $totalCombos-$combosEntregados }},{{ $combosEntregados }}],
                                     backgroundColor: [
-                                        "rgb(34, 197, 94)",
                                         "rgb(239, 68, 68)",
+                                        "rgb(34, 197, 94)",
                                     ],
                                     hoverOffset: 4,
                                     },
