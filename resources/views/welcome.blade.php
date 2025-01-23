8bc573994b406dcb0f76cf40bbc4b78b91082ca2
sudacastream <ceo@sudacastream.com>
2025-01-22 23:44:14 -0300

    2025

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,17 +4,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Congreso Direccionados 2025</title>
-        <meta name="title" content="Congreso Direccionados 2024" />
-        <meta name="description" content="Congreso de Jóvenes Direccionados: 18 de mayo de 2024 en el Club Parque Sur de Concepci&oacute;n del Uruguay." />
+        <meta name="title" content="Congreso Direccionados 2025" />
+        <meta name="description" content="Congreso de Jóvenes Direccionados: 17 de mayo de 2025 en el Club Parque Sur de Concepci&oacute;n del Uruguay." />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://direccionados.ar/" />
-        <meta property="og:title" content="Congreso Direccionados 2024" />
-        <meta property="og:description" content="Congreso de Jóvenes Direccionados: 18 de mayo de 2024 en el Club Parque Sur de Concepci&oacute;n del Uruguay." />
+        <meta property="og:title" content="Congreso Direccionados 2025" />
+        <meta property="og:description" content="Congreso de Jóvenes Direccionados: 17 de mayo de 2025 en el Club Parque Sur de Concepci&oacute;n del Uruguay." />
         <meta property="og:image" content="https://direccionados.ar/congreso-direccionados.jpg" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://direccionados.ar/" />
-        <meta property="twitter:title" content="Congreso Direccionados" />
-        <meta property="twitter:description" content="Congreso de Jóvenes Direccionados: 18 de mayo de 2024 en el Club Parque Sur de Concepci&oacute;n del Uruguay." />
+        <meta property="twitter:title" content="Congreso Direccionados 2025" />
+        <meta property="twitter:description" content="Congreso de Jóvenes Direccionados: 17 de mayo de 2025 en el Club Parque Sur de Concepci&oacute;n del Uruguay." />
         <meta property="twitter:image" content="https://direccionados.ar/congreso-direccionados.jpg" />
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <link rel="preconnect" href="https://fonts.googleapis.com">
