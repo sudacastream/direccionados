f6a70dd2dda440e4caa194e27f0abbdb8c92a09c
sudacastream <ceo@sudacastream.com>
2025-01-28 13:35:01 -0300

    Muy pronto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,19 +19,45 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Climate+Crisis&family=Cute+Font&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
     </head>
     <style>
-        .font-climate
+        .font-press
         {
-            font-family: 'Climate Crisis', sans-serif;
+            font-family: "Press Start 2P", serif;
+            font-weight: 400;
+            font-style: normal;
         }
-        .font-cute
+        .px-general
         {
-            font-family: 'Cute Font', sans-serif;
+        padding-left: 10px;
+        padding-right: 10px;
+        }
+        @media (min-width: 768px) 
+        {
+            .px-general
+            {
+                padding-left: 30px;
+                padding-right: 30px;
+            }
+        }
+        @media (min-width: 992px) 
+        {
+            .px-general
+            {
+                padding-left: 40px;
+            }
         }
     </style>
     <body class="antialiased">
-        MMM...
+        <header class="w-full h-screen" style="background:url(/bg.jpg) center center no-repeat;background-size:cover;">
+            <img class="w-20 fixed left-1/2 -translate-x-1/2 top-2" src="/direccionados-white.png" alt="Isologo Direccionados">
+            <h1 class="font-press text-xs uppercase text-transparent tracking-widest fixed bottom-2 w-full text-center">Congreso Direccionados</h1>
+            <div class="px-general w-full md:w-2/3 fixed top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 grid gap-6 justify-center">
+                <img class="w-full" src="/logotipo-direccionados.png" alt="Logotipo Direccionados">
+                <h2 class="font-press uppercase text-white text-2xl md:text-3xl tracking-widest text-center">Muy pronto</h2>
+            </div>
+            <img class="w-28 fixed bottom-3 left-1/2 -translate-x-1/2" src="/icons-3.png" alt="Iconos">
+        </header>
     </body>
 </html>
