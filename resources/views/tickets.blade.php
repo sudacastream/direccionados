eb7f42904316b7956dbe0ce231d42834a82030c5
sudacastream <ceo@sudacastream.com>
2025-02-01 13:11:24 -0300

    quita de 10

--- resources/views/tickets.blade.php
+++ resources/views/tickets.blade.php
@@ -28,7 +28,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="flex items-center p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50" role="alert">
+                <!--<div class="flex items-center p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50" role="alert">
                   <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
                   </svg>
@@ -36,7 +36,7 @@
                   <div>
                     <span class="font-medium">¡Aprovech&aacute;!</span> Comprando diez (10) tickets, ten&eacute;s uno (1) sin cargo.
                   </div>
-                </div>
+                </div>-->
                 <div>
                   <h2 class="my-4 text-lg font-medium text-gray-900 ">Datos de procedencia</h2>
                   <div class="flex flex-wrap px-3">
