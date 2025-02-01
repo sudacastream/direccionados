b5fadb726df458a2129c115559e13ab6dfc06f90
sudacastream <ceo@sudacastream.com>
2025-02-01 13:08:37 -0300

    correccion

--- resources/views/tickets.blade.php
+++ resources/views/tickets.blade.php
@@ -6,7 +6,7 @@
         </h2>
     </x-slot>
     
-    @if(Auth::user()->email!='ceo@sudacastream.com')
+    @if(Auth::user()->email=='aassceo@sudacastream.com')
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
           <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">            
